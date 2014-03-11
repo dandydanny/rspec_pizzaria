6ef9c85fb821620e140ceddf2580bdccb0d526dd
jdubnicek <jdubnicek@gmail.com>
2014-03-11 14:37:25 -0500

    NOTHING

--- rspec_class_pizza.rb
+++ rspec_class_pizza.rb
@@ -1,5 +1,4 @@
 class Pizza
-<<<<<<< HEAD
 	attr_reader :name, :description, :time_baked
 
 	def initialize(args)
@@ -9,12 +8,3 @@ class Pizza
 	end
 end
 
-=======
-
-	attr_reader :name
-
-	def initialize(args)
-		@name = args[:name]
-	end
-end
->>>>>>> 76f6f116e0a348d884bb6ec3d55bcdb96a13357a
